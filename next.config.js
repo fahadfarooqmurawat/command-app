7cf6fae66d0ce36035a83b3412cc2393c2e50989
fahadfarooqmurawat <fahadfarooqmurawat@gmail.com>
2023-12-20 16:58:19 +0500

    test different configs

--- next.config.js
+++ next.config.js
@@ -1,10 +1,13 @@
+const TerserPlugin = require("terser-webpack-plugin");
+
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+  // swcMinify: false,
   webpack: (config) => {
-    // Modify the webpack config here
-    config.optimization = {
-      minimize: false,
-    };
+    // config.optimization = {
+      // minimize: false,
+    //   minimizer: [new TerserPlugin({ terserOptions: { mangle: false } })] // mangle false else mysql blow ups with "PROTOCOL_INCORRECT_PACKET_SEQUENCE"
+    // };
 
     return config;
   },
